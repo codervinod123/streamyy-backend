37647c5131b850b8e981ef39a47613a51db79b09
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2023-11-22 16:25:45 +0530

    setup completed

--- db/index.js
+++ db/index.js
@@ -6,10 +6,11 @@ import { DB_name } from "../constants.js";
 const connect_DB=async()=>{
     try {
         const connectingInstance=await mongoose.connect("mongodb+srv://vinodpvp9369:4BF5NKWcR8YhRCme@cluster0.fj3v3gb.mongodb.net/youtube");
-        console.log("Connected successfully");
+        console.log("Connected successfully on port number 4000");
     } catch (error) {
           console.log(error)
           process.exit(1);
+
     }
 }
 
